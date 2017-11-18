3dbacb7ba774887217c278130c44d976d7f12a72
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2017-11-18 11:27:39 +0700

    Proof of golang interface concept.

--- timestamp/timesheet_test.go
+++ timestamp/timesheet_test.go
@@ -1,7 +1,49 @@
 package timestamp
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"time"
+)
 
-func TestOntime(t *testing.T) {
+type H interface {
+	Hour() int
+}
 
+type realTime struct {
+}
+
+func (r realTime) Hour() int {
+	return time.Now().Hour()
+}
+
+type fakeTime struct {
+}
+
+func (f fakeTime) Hour() int {
+	return 8
+}
+
+type Timesheet struct {
+	H
+}
+
+func (t Timesheet) Record(s string) {
+	result := "Late"
+	if t.Now() < 9 {
+		result = "OnTime"
+	}
+
+	fmt.Println(s, " | ", result)
+}
+
+func (t Timesheet) Now() int {
+	return t.Hour()
+}
+
+func TestOnTimeBefore9AM(t *testing.T) {
+	timesheet := Timesheet{H: new(fakeTime)}
+	//timesheet := Timesheet{H: new(realTime)}
+
+	timesheet.Record("KWAN")
 }
